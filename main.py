faf42655fc8fef96d03ca6f2178c6b91739983e3
Tony Seing <_@tonyseing.com>
2018-04-13 01:48:11 +0800

    adds ability to choose output file name

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ def main():
         if len(sys.argv) <= 1:
             raise FileNotSpecified("No input file specified")
 
+        elif len(sys.argv) <= 2:
+            raise FileNotSpecified("No output file specified")
+
         image_name = sys.argv[1]
+        output_name = sys.argv[2]
 
         # read in the image
         image = cv2.imread(image_name, cv2.IMREAD_COLOR)
@@ -21,17 +25,19 @@ def main():
         painted_image = paint(image)
 
         # display it for testing
+        """
         cv2.imshow('image', painted_image)
         cv2.namedWindow('image', cv2.WINDOW_NORMAL)
         cv2.resizeWindow('image', 1200, 800)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+        """
 
         # write image to disk
-        cv2.imwrite('output.jpg', painted_image)
+        cv2.imwrite(output_name, painted_image)
 
     except FileNotSpecified:
-        print "Error: File not specified"
+        print "Error: Either input or output file not specified"
         sys.exit()
 
 class FileNotSpecified(Exception):
